9c098cc24f2b234e1da418eb606f10c676f5e562
Eric Chen <cgx1997@outlook.com>
2017-11-13 21:51:58 -0800

    modified System.m, unit conversion

--- System.m
+++ System.m
@@ -20,9 +20,66 @@
 % ==========================
 % Choose Motors
 % ==========================
+AMAX22_5W_SB;                % Default Maxon motor
+Q0 = MotorParam;
+Q1 = MotorParam;
 
 % Motor Unit Conversions
-% ----------------------
+ConversionArray = ...
+...             
+[1                      % NomV          (V)
+ RadPSecPerRPM          % NoLoadSpd     (rpm)
+ 0.001                  % NoLoadCurr    (mA)
+ RadPSecPerRPM          % NomSpd        (rpm)
+ 0.001                  % NomTorque     (mNm)
+ 0.001                  % NomCurr       (mA)
+ 0.001                  % StallTorque   (mNm)
+ 1                      % StallCurr     (A)
+ 0.01                   % MaxEff        (%)
+...
+...                     % Characteristics
+ 1                      % TermR         (Ohms)
+ 0.001                  % TermL         (mH)
+ 0.001                  % TorqueConst   (mNm/A)
+ RadPSecPerRPM          % SpdConst      (rpm/V)
+ RadPSecPerRPM*1000     % SpdTorqueGrad (rpm/mNm)
+ 0.001                  % MechTimeConst (ms)
+ 1e-7                   % RotJ          (gcm^2)
+...
+...                     % Thermal Data
+ 1                      % ThermRhous    (K/W)
+ 1                      % ThermRwind    (K/W)
+ 1                      % ThermTCwind   (s)
+ 1                      % ThermTCmot    (s) 
+ 1                      % AmbTemp       (degC) --NEED LATER CONVERSION
+ 1                      % MaxTemp       (degC) --NEED LATER CONVERSION
+...
+...                     % Mechanical Data
+ RadPSecPerRPM          % MaxSpd        (rpm)
+ 0.001                  % AxialPlay     (mm)
+ 0.001                  % RadPlay       (mm)
+ 1                      % MaxAxLd       (N)
+ 1                      % MaxF          (N)
+ 1                      % MaxRadLd      (N)
+...
+...                     % Other Specifications
+ 1                      % NoPolePair    (pure)
+ 1                      % NoCommSeg     (pure)
+ 0.001                  % Weight        (g)
+...
+...                     % Physical Dimensions
+ 0.001                  % OuterDiam     (mm)
+ 0.001];                % Length        (mm)
+
+% Convert units to standard units
+Q0 = ConversionArray .* Q0; 
+Q1 = ConversionArray .* Q1;
+
+% Convert temperatures to Kelvin
+Q0(21) = Q0(21)+273; 
+Q1(21) = Q1(21)+273;
+Q0(22) = Q0(22)+273;
+Q1(22) = Q1(22)+273;
 
 
 % ==========================
@@ -30,8 +87,10 @@
 % ==========================
 
 % Maximum Current
-% ---------------
-
+NomI0   = 1;                 % Max average current
+StallI0 = 1;                 % Max peak current
+NomI1   = 1;                 % Max average current
+StallI1 = 1;                 % Max peak current
 
 % =============================
 % Q0 : Rotation about y-axis
@@ -44,7 +103,7 @@
 % -------------------------
 
 % Torque Const & Back EMF
-% -----------------------
+TConst0  = Q0(12);
 
 % Mechanical Motor Dynamics
 % -------------------------
@@ -67,7 +126,7 @@
 % -------------------------
 
 % Torque Const & Back EMF
-% -----------------------
+TConst1  = Q1(12);
 
 % Mechanical Motor Dynamics
 % -------------------------
